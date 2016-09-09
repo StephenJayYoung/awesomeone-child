fa7821ee2939855d1f06748b5d8ca5712ad060d9
Stephen Young <stephenjayyoung@gmail.com>
2016-09-09 15:19:17 -0700

    added bootstrap

--- functions.php
+++ functions.php
@@ -1,2 +1,9 @@
 <?php
 
+wp_register_script( 'bootstrap-js', get_template_directory_uri() . '/bootstrap/bootstrap.min.js', array( 'jquery' ), '3.0.1', true );
+
+wp_register_style( 'bootstrap-css', get_template_directory_uri() . '/bootstrap/bootstrap.min.css', array(), '3.0.1', 'all' );
+
+wp_enqueue_script( 'bootstrap-js' );
+
+wp_enqueue_style( 'bootstrap-css' );
--- header.php
+++ header.php
@@ -13,8 +13,10 @@
 <meta name="viewport" content="width=device-width">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
 <?php wp_head(); ?>
 </head>
+<link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet">
 
 <body <?php body_class(); ?>>
 <div class="header" <?php if(!is_home() || !is_front_page()) {?> style="position:inherit;"<?php }?>>
