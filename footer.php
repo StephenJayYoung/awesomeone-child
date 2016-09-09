adf88c97be16cd650d44f8f4d34c570b0a596e86
Stephen Young <stephenjayyoung@gmail.com>
2016-09-09 10:30:18 -0700

    first commit

---
+++ footer.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Awesomeone
+ */
+?>
+
+        
+        <div class="copyright-wrapper text-center">
+        	<div class="inner col-sm-12">
+                <div class="copyright text-center">
+                    	<p><?php echo awesomeone_credit_link(); ?></p>               
+                </div><!-- copyright --><div class="clear"></div>         
+        </div>
+    </div>
+<?php wp_footer(); ?>
+
+</body>
+</html>
