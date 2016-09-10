644170ccc5b36ba395c2406905fade5f601daf01
Stephen Young <stephenjayyoung@gmail.com>
2016-09-10 15:26:50 -0700

    additions to functions.php

--- functions.php
+++ functions.php
@@ -1,9 +1,25 @@
 <?php
 
-wp_register_script( 'bootstrap-js', get_template_directory_uri() . '/bootstrap/bootstrap.min.js', array( 'jquery' ), '3.0.1', true );
-
+function theme_styles() {
 wp_register_style( 'bootstrap-css', get_template_directory_uri() . '/bootstrap/bootstrap.min.css', array(), '3.0.1', 'all' );
+}
 
-wp_enqueue_script( 'bootstrap-js' );
+//Load the theme JS
+function theme_js() {
+    //Adds JQuery from Google's CDN. Code pulled from www.http://css-tricks.com/snippets/wordpress/include-jquery-in-wordpress-theme/
+    if (!is_admin()) add_action("wp_enqueue_scripts", "my_jquery_enqueue", 11);
+    function my_jquery_enqueue() {
+    wp_deregister_script('jquery');
+    wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js", false, null);
+    wp_enqueue_script('jquery');
+}
+    wp_enqueue_script( 'bootstrapjs', get_template_directory_uri() . '/bootstrap/bootstrap.min.js', array('jquery'), '', true );
+}
 
-wp_enqueue_style( 'bootstrap-css' );
+wp_enqueue_script( 'theme_styles' );
+
+wp_enqueue_style( 'theme_js' );
+
+
+?>
+
